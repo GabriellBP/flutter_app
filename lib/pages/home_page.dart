fb10c811874d4f94d1c21481d3d00073a21a6524
gabriel <gbp@ic.ufal.br>
2019-11-17 14:20:09 -0300

    working with scroll view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,14 +17,15 @@ class HomePage extends StatelessWidget {
   _body(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      color: Colors.green,
-      padding: const EdgeInsets.all(16),
+    return SingleChildScrollView(
       child: Container(
         color: Colors.yellow,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
+            _text(),
+            _pageView(),
+            _buttons(),
             _text(),
             _pageView(),
             _buttons()
