b9e32c18de65fffbeca5a455f0f986853ff049aa
gabriel <gbp@ic.ufal.br>
2020-01-07 18:40:18 -0300

    working with alignment

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -34,9 +34,12 @@ class HelloListView extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             _img(dog.foto),
-            Text(
-              dog.nome,
-              style: TextStyle(fontSize: 26, color: Colors.white),
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                dog.nome,
+                style: TextStyle(fontSize: 26, color: Colors.white),
+              ),
             )
           ],
         );
