021f3ccbba4b0fd8ff7e4a6d5d785c93e5364c39
gabriel <gbp@ic.ufal.br>
2020-01-08 20:53:06 -0300

    implementing Snack alert

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,87 +22,101 @@ class HomePage extends StatelessWidget {
   }
 
   _body(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
 
     return Container(
       color: Colors.yellow,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
-          _text(),
-          _pageView(),
-          _buttons(context),
-        ]
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _text(),
+            _pageView(),
+            _buttons(),
+          ]
       ),
     );
   }
 
   _pageView() {
     return Container(
-        margin: EdgeInsets.symmetric(vertical: 20),
-        height: 300,
-        child: PageView(
-          children: <Widget>[
-            _img("assets/images/dog1.png"),
-            _img("assets/images/dog2.png"),
-            _img("assets/images/dog3.png"),
-            _img("assets/images/dog4.png"),
-            _img("assets/images/dog5.png"),
-          ],
-        ),
+      margin: EdgeInsets.symmetric(vertical: 20),
+      height: 300,
+      child: PageView(
+        children: <Widget>[
+          _img("assets/images/dog1.png"),
+          _img("assets/images/dog2.png"),
+          _img("assets/images/dog3.png"),
+          _img("assets/images/dog4.png"),
+          _img("assets/images/dog5.png"),
+        ],
+      ),
+    );
+  }
+
+  _buttons() {
+    return Builder(builder: (context) {
+      return Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              BlueButtom(
+                  "ListView",
+                  onPressed: () => _onClickNavigator(context, HelloListView())
+              ),
+              BlueButtom(
+                  "Page 2",
+                  onPressed: () {
+                    _onClickNavigator(context, HelloPage2());
+                  }
+              ),
+              BlueButtom(
+                  "Page 3",
+                  onPressed: () => _onClickNavigator(context, HelloPage3())
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              BlueButtom(
+                  "Snack",
+                  onPressed: () => _onClickSnack(context)
+              ),
+              BlueButtom(
+                  "Dialog",
+                  onPressed: _onClickDialog
+              ),
+              BlueButtom(
+                  "Toast",
+                  onPressed: _onClickToast
+              ),
+            ],
+          ),
+        ],
       );
+    },);
   }
 
-  Column _buttons(context) {
-    return Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                BlueButtom(
-                    "ListView",
-                    onPressed: () => _onClickNavigator(context, HelloListView())
-                ),
-                BlueButtom(
-                    "Page 2",
-                    onPressed: () {
-                      _onClickNavigator(context, HelloPage2());
-                    }
-                ),
-                BlueButtom(
-                    "Page 3",
-                    onPressed: () => _onClickNavigator(context, HelloPage3())
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                BlueButtom(
-                    "Snack",
-                    onPressed: _onClickSnack
-                ),
-                BlueButtom(
-                    "Dialog",
-                    onPressed: _onClickDialog
-                ),
-                BlueButtom(
-                    "Toast",
-                    onPressed: _onClickToast
-                ),
-              ],
-            ),
-          ],
-        );
-  }
-
-  _onClickNavigator(context, Widget page) async{
+  _onClickNavigator(context, Widget page) async {
     String s = await push(context, page);
 
     print(">> $s");
   }
 
-  _onClickSnack() {
+  _onClickSnack(context) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text("Olá Futter"),
+      action: SnackBarAction(
+        textColor: Colors.deepPurple,
+        label: "Ok",
+        onPressed: () {
+          print("OK SnackBar");
+        },
+      ),
+    ));
   }
 
   _onClickDialog() {
@@ -114,23 +128,23 @@ class HomePage extends StatelessWidget {
   _img(String image) {
 //    return Image.network("https://thehappypuppysite.com/wp-content/uploads/2017/10/Cute-Dog-Names-HP-long.jpg");
     return Image.asset(
-        image,
-        fit: BoxFit.cover,
+      image,
+      fit: BoxFit.cover,
     );
   }
 
   _text() {
     return Text(
-        "Hello World",
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.red,
-          fontWeight: FontWeight.bold,
-          decoration: TextDecoration.underline,
-          decorationColor: Colors.blue,
-          decorationStyle: TextDecorationStyle.wavy,
+      "Hello World",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.blue,
+        decorationStyle: TextDecorationStyle.wavy,
 
-        ),
-      );
+      ),
+    );
   }
 }
